0cd2ed9d85ff231dcf22561c1c8b5201841092d2
Danil <danchief@yandex.ru>
2021-08-11 01:49:17 +0300

    Created

--- src/ru/test/printers/Printer1.java
+++ src/ru/test/printers/Printer1.java
@@ -1,7 +1,8 @@
-package java.ru.test.printers;
+package ru.test.printers;
 
 public class Printer1 {
     public void printText(String str) {
+        System.out.println("Печатает принтер 1...");
         System.out.println("Text for printing: " + str);
     }
 }
--- src/ru/test/printers/Printer3.java
+++ src/ru/test/printers/Printer3.java
@@ -1,7 +1,8 @@
-package java.ru.test.printers;
+package ru.test.printers;
 
 public class Printer3 {
     public void printText228(String str) {
+        System.out.println("Печатает принтер 3...");
         String printText;
         if (str != null) {
             printText = "Text for printing: ";
